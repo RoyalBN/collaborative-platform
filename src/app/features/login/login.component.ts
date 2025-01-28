849891a9532285e760992b59935f4b27e6d6ca45
RoyalBN <sebastien.samson22@gmail.com>
2025-01-28 20:41:55 +0100

    création page login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    CardModule,
+    ButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private formBuilder = inject(FormBuilder);
+
+  profileForm = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  })
+
+  login() {
+  
+  }
+    
 
 }
